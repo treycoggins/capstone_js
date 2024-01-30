25ac6e05bfa1a16d8235c6e758c328f2ae7fce98
Trey Coggins <trey@surfturkey.net>
2024-01-30 06:50:47 -0500

    built out sign in page

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@
 const navMenu = document.getElementById('hamburger');
 const navPane = document.getElementById('nav');
 const mainBody = document.getElementById('overlay');
-const navMenuClose = document.getElementById('navMenu-close');
+const navMenuCloseBtn = document.getElementById('navMenu-close');
 
+const navLinks = [...document.getElementsByClassName('nav-link-item')];
 
-navMenu.addEventListener('click', () => {
+const navMenuOpen = () => {
   navPane.classList.remove('nav-close');
   navPane.classList.toggle('nav-open');
   mainBody.classList.toggle('overlay-open');
-});
+};
 
-navMenuClose.addEventListener('click', () => {
+const navMenuClose = () => {
   navPane.classList.remove('nav-open');
   navPane.classList.add('nav-close');
   mainBody.classList.remove('overlay-open');
-})
+};
+
+navMenu.addEventListener('click', navMenuOpen);
+navMenuCloseBtn.addEventListener('click', navMenuClose)
+navLinks.forEach(navLink => navLink.addEventListener('click', navMenuClose));
 
 const findItems = () => {
   const item = document.getElementById('main-search-field').value;
--- src/signup.js
+++ src/signup.js
@@ -1,6 +1,12 @@
 const createSection = document.getElementById("section-create");
 const signinSection = document.getElementById("section-signin");
+const signupForm = document.getElementById("signup-form");
+const signinForm = document.getElementById("signin-form");
+
 
 createSection.addEventListener("click", () => {
-	console.log('clicked')
+	signupForm.classList.toggle('show-form');
+});
+signinSection.addEventListener("click", () => {
+	signinForm.classList.toggle('show-form');
 });
